a50930ebdd4fd2ce434441f7cd8f75d66714f638
Hariharan <hariharansriram17@gmail.com>
2022-01-02 12:58:44 +0000

    Fixed Cors Error

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,14 @@ import EmployerRoutes from "./Routes/EmployerRoutes";
 const app = express();
 dotEnv.config();
 app.use(bodyParser.json());
+app.options("*", (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL? process.env.FRONTEND_URL : "");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+    res.sendStatus(200);
+});
 app.use((req, res, next) => {
+    // console.log(req.method, req.path);
     res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL? process.env.FRONTEND_URL : "");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-type,Authorization');
